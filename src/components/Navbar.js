6c271dce59151e4f47d3be85e8dd98ad2191412e
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-01-30 16:49:22 +0100

    Navbar uradjen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import Logo from '../assets/carLogo.png';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import '../styles/Navbar.css';
 
 function Navbar() {
-    return <div className = 'navbar' >
-        <
-        div className = 'leftside' >
-        <
-        img src = { Logo }
-    /> < /
-    div > <
-        div className = 'rightside' >
-        <
-        Link to = "/" > Home < /Link> <
-    Link to = "/cars" > Cars < /Link> <
-    Link to = "/about" > About < /Link> <
-    Link to = "/contact" > Contact < /Link>
+  return <div className='navbar'>
+<div className='leftside'>
+    <img src={Logo}/>
+</div>
+<div className='rightside'>
+<Link to="/">Home</Link>
+<Link to="/cars">Cars</Link>
+<Link to="/about">About</Link>
+<Link to="/contact">Contact</Link>
 
 
-    <
-    /div> < /
-    div > ;
+</div>
+  </div>;
 }
 
 export default Navbar;
