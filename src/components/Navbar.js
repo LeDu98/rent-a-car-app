eb02e297cfdf658430bbba230c69ddc38abc80a9
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-01-30 16:48:46 +0100

    Uradjen Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from '../assets/carLogo.png';
+import { Link } from "react-router-dom";
+import '../styles/Navbar.css';
+
+function Navbar() {
+    return <div className = 'navbar' >
+        <
+        div className = 'leftside' >
+        <
+        img src = { Logo }
+    /> < /
+    div > <
+        div className = 'rightside' >
+        <
+        Link to = "/" > Home < /Link> <
+    Link to = "/cars" > Cars < /Link> <
+    Link to = "/about" > About < /Link> <
+    Link to = "/contact" > Contact < /Link>
+
+
+    <
+    /div> < /
+    div > ;
+}
+
+export default Navbar;
