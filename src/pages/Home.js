a21162e92c83d9fb08ab7cbd5a1cf6ace8d7794b
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-01-30 17:05:23 +0100

    Kreirana home page sa Banner Image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import BannerImage from "../assets/carsWallper.jpg";
+import "../styles/Home.css";
 
 function Home() {
-  return <div> <h1>HELOOOOOO</h1></div>;
+  return <div className='home'> 
+    <div className='headerContainer' style={{backgroundImage: `url(${BannerImage})`}}> 
+      <h1>Rent-a-Car MP</h1>
+      <p>We have car for everyone!</p>
+      <Link to="/cars">
+        <button>RENT NOW</button>
+      </Link>
+    </div>
+  </div>;
 }
 
 export default Home;
